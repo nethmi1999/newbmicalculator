6ef36b5af4e24ee19576fb8b398028e088cb4964
tasheela <tasheelajay1999@gmail.com>
2023-08-17 19:31:14 +0530

    bugs fixed

--- lib/authentication/login.dart
+++ lib/authentication/login.dart
@@ -35,106 +35,104 @@ class BackgroundImageWidget extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Form(
-                    key: _fromKey,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              'assets/images/login.jpg',
-                              width: 250,
-                              alignment: Alignment.center,
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Form(
+                  key: _fromKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/login.jpg',
+                            width: 250,
+                            alignment: Alignment.center,
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          SizedBox(height: 10.0),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Enter your email',
                             ),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            SizedBox(height: 10.0),
-                            TextFormField(
-                              decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                                labelText: 'Enter your email',
-                              ),
 
-                              controller: emailController,
-                              onSaved: (value) {
-                                email = value;
-                              },
+                            controller: emailController,
+                            onSaved: (value) {
+                              email = value;
+                            },
+                          ),
+                          SizedBox(height: 10.0),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Enter your password',
                             ),
-                            SizedBox(height: 10.0),
-                            TextFormField(
-                              decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                                labelText: 'Enter your password',
-                              ),
-                              controller: passwordController,
+                            controller: passwordController,
 
-                              onSaved: (value) {
-                                password = value;
-                              },
-                            ),
-                            SizedBox(height: 15.0),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                          backgroundColor:
-                                          Color.fromARGB(255, 159, 34, 231),
-                                          elevation: 0,
-                                          padding: EdgeInsets.all(20)),
-                                      onPressed: () {
-                                        FirebaseAuth.instance
-                                            .signInWithEmailAndPassword(
-                                            email: emailController.text,
-                                            password: passwordController.text);
-                                      },
-                                      child: const Text(
-                                        "Login",
-                                        style: TextStyle(
-                                          fontSize: 20.0,
-                                          // Adjust the font size as needed
-                                          fontWeight: FontWeight.bold,
-                                          color: Color.fromARGB(255, 249, 249, 250),
-                                        ),
-                                      )),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        const Column(
-                          children: [Text("OR"), Text("Don't Have an Account?")],
-                        ),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                      backgroundColor: Colors.purple.withOpacity(0.9),
-                                      elevation: 0,
-                                      padding: EdgeInsets.all(20)),
-                                  onPressed: () => Get.to(() => RegisterScreen()),
-                                  child: const Text(
-                                    "Register",
-                                    style: TextStyle(
-                                      fontSize: 20.0, // Adjust the font size as needed
-                                      fontWeight: FontWeight.bold,
-                                      color: Color.fromARGB(255, 249, 249,
-                                          250), // Change the text color if desired
-                                    ),
-                                  )),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
+                            onSaved: (value) {
+                              password = value;
+                            },
+                          ),
+                          SizedBox(height: 15.0),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                        backgroundColor:
+                                        Color.fromARGB(255, 159, 34, 231),
+                                        elevation: 0,
+                                        padding: EdgeInsets.all(20)),
+                                    onPressed: () {
+                                      FirebaseAuth.instance
+                                          .signInWithEmailAndPassword(
+                                          email: emailController.text,
+                                          password: passwordController.text);
+                                    },
+                                    child: const Text(
+                                      "Login",
+                                      style: TextStyle(
+                                        fontSize: 20.0,
+                                        // Adjust the font size as needed
+                                        fontWeight: FontWeight.bold,
+                                        color: Color.fromARGB(255, 249, 249, 250),
+                                      ),
+                                    )),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const Column(
+                        children: [Text("OR"), Text("Don't Have an Account?")],
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.purple.withOpacity(0.9),
+                                    elevation: 0,
+                                    padding: EdgeInsets.all(20)),
+                                onPressed: () => Get.to(() => RegisterScreen()),
+                                child: const Text(
+                                  "Register",
+                                  style: TextStyle(
+                                    fontSize: 20.0, // Adjust the font size as needed
+                                    fontWeight: FontWeight.bold,
+                                    color: Color.fromARGB(255, 249, 249,
+                                        250), // Change the text color if desired
+                                  ),
+                                )),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
               )
--- lib/authentication/register.dart
+++ lib/authentication/register.dart
@@ -47,158 +47,156 @@ class _BackgroundImageWidgetState extends State<BackgroundImageWidget> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.all(40.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            'assets/images/register.jpg',
-                            width: 250,
-                            alignment: Alignment.center,
+              Padding(
+                padding: const EdgeInsets.all(40.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/register.jpg',
+                          width: 250,
+                          alignment: Alignment.center,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Register Now!',
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
                           ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            'Register Now!',
-                            style: TextStyle(
-                              fontSize: 40,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 25.0),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Enter Your Name',
                             ),
+                            controller: nameController,
+                            onSaved: (value) {
+                              name = value;
+                            },
                           ),
-                        ],
-                      ),
-                      SizedBox(height: 25.0),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: TextFormField(
-                              decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                                labelText: 'Enter Your Name',
-                              ),
-                              controller: nameController,
-                              onSaved: (value) {
-                                name = value;
-                              },
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10.0),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Enter your Phone Number',
                             ),
+                            controller: phoneController,
+                            onSaved: (value) {
+                              phonenumber = value;
+                            },
                           ),
-                        ],
-                      ),
-                      SizedBox(height: 10.0),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: TextFormField(
-                              decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                                labelText: 'Enter your Phone Number',
-                              ),
-                              controller: phoneController,
-                              onSaved: (value) {
-                                phonenumber = value;
-                              },
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10.0),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Enter your Email',
                             ),
+                            controller: emailController,
+                            onSaved: (value) {
+                              email = value;
+                            },
                           ),
-                        ],
-                      ),
-                      SizedBox(height: 10.0),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: TextFormField(
-                              decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                                labelText: 'Enter your Email',
-                              ),
-                              controller: emailController,
-                              onSaved: (value) {
-                                email = value;
-                              },
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10.0),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Enter your Password',
                             ),
+                            controller: passwordController,
+                            onSaved: (value) {
+                              password = value;
+                            },
                           ),
-                        ],
-                      ),
-                      SizedBox(height: 10.0),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: TextFormField(
-                              decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                                labelText: 'Enter your Password',
-                              ),
-                              controller: passwordController,
-                              onSaved: (value) {
-                                password = value;
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 10.0),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                    backgroundColor: Colors.purple.withOpacity(0.9),
-                                    elevation: 0,
-                                    padding: EdgeInsets.all(20)),
-                                onPressed: (){
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10.0),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.purple.withOpacity(0.9),
+                                  elevation: 0,
+                                  padding: EdgeInsets.all(20)),
+                              onPressed: (){
 
-                                  FirebaseAuth.instance.createUserWithEmailAndPassword(email: emailController.text, password: passwordController.text)
-                                      .whenComplete(() => Get.to(()=>LoginScreen()));
+                                FirebaseAuth.instance.createUserWithEmailAndPassword(email: emailController.text, password: passwordController.text)
+                                    .whenComplete(() => Get.to(()=>LoginScreen()));
 
-                                },
-                                child: const Text(
-                                  "Register",
-                                  style: TextStyle(
-                                    fontSize: 20.0, // Adjust the font size as needed
-                                    fontWeight: FontWeight.bold,
-                                    color: Color.fromARGB(255, 249, 249,
-                                        250), // Change the text color if desired
-                                  ),
-                                )),
-                          ),
-                        ],
-                      ),
-                      const Column(
-                        children: [
-                          Text("OR"),
-                          Text("Already Have an Account?")
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                    backgroundColor: Colors.purple.withOpacity(0.5),
-                                    elevation: 0,
-                                    padding: EdgeInsets.all(20)),
-                                onPressed: ()=>Get.to(()=>LoginScreen()),
-                                child: const Text(
-                                  "Login",
-                                  style: TextStyle(
-                                    fontSize: 20.0, // Adjust the font size as needed
-                                    fontWeight: FontWeight.bold,
-                                    color: Color.fromARGB(255, 249, 249,
-                                        250), // Change the text color if desired
-                                  ),
-                                )),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+                              },
+                              child: const Text(
+                                "Register",
+                                style: TextStyle(
+                                  fontSize: 20.0, // Adjust the font size as needed
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 249, 249,
+                                      250), // Change the text color if desired
+                                ),
+                              )),
+                        ),
+                      ],
+                    ),
+                    const Column(
+                      children: [
+                        Text("OR"),
+                        Text("Already Have an Account?")
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.purple.withOpacity(0.5),
+                                  elevation: 0,
+                                  padding: EdgeInsets.all(20)),
+                              onPressed: ()=>Get.to(()=>LoginScreen()),
+                              child: const Text(
+                                "Login",
+                                style: TextStyle(
+                                  fontSize: 20.0, // Adjust the font size as needed
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 249, 249,
+                                      250), // Change the text color if desired
+                                ),
+                              )),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ],
